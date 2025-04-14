bd86d7cd2d7fc48d6866cb15ba7b16b98f702117
KJW-623 <kimjiwoo4056@gmail.com>
2025-04-14 11:35:36 +0900

    자바 시작, gitignore 추가

---
+++ src/study/start/Hello.java
@@ -0,0 +1,19 @@
+package study.start;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!!");
+		
+		// 주석
+		// 실행(Run) : Ctrl + F11
+		
+		/* 주석 단축키 : Ctrl + / (여러줄 안됨)
+					  Ctrl + Shift + c (여러줄 됨)
+		 * 여러줄 주석
+		 *   
+		 */
+	}
+
+}
