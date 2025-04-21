5c252bd6ff8ef2a71fc5221910f2640d3bdb4a53
KJW-623 <kimjiwoo4056@gmail.com>
2025-04-21 15:59:13 +0900

    참조변수, 배열

---
+++ src/study/refer/Reference01.java
@@ -0,0 +1,47 @@
+package study.refer;
+
+import java.util.Scanner;
+
+public class Reference01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//참조변수
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		String str1 = "아침";
+		String str2 = "아침";
+		String str3 = scanner.nextLine();
+		
+		System.out.println(str1==str2);	//참조변수에서 == 비교는 값비교가 아니라 참조하는 주소가 같은지 비교하는 것
+		System.out.println(str1==str3);
+		System.out.println(str2.equals(str3));
+		System.out.println(str3.equals(str2));
+
+		//String 값을 비교할 때는 equals를 쓰자
+		
+		//null
+		int num;
+		int sum = 0;
+		
+		//num=num+10;
+		sum=sum+10;
+	
+		//System.out.println(num);
+		System.out.println(sum);
+		
+		String s1;
+		String s2 = null; // 참조하는 게 없다!
+		String s3 = "글자";
+		
+		//System.out.println(s1);
+		System.out.println(s2); //null
+		System.out.println(s3);
+		
+		if(s2 != null)
+			System.out.println(s2.equals("글자"));
+	}
+
+}
---
+++ src/study/refer/Reference04.java
@@ -0,0 +1,33 @@
+package study.refer;
+
+public class Reference04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int[] arr1 = {1,2,3};
+		int[] arr2 = {1,2,3};
+		int[] arr22 = arr2;
+		
+		System.out.println(arr1==arr2);
+		System.out.println(arr1[1]==arr2[1]);
+		
+		System.out.println(arr2==arr22);
+		
+		String[] arr3 = {"A", "B", "c"};
+		String[] arr4 = {"A", "B", "c"};
+		
+		System.out.println(arr3==arr4);
+		//System.out.println(arr3[0]==arr4[0]);
+		System.out.println(arr3[0].equals(arr4[0]));
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
