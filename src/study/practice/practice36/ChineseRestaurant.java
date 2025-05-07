d20cb124231b1becd3bb5c4943daf60314b2a2f6
KJW-623 <kimjiwoo4056@gmail.com>
2025-05-07 17:07:50 +0900

    연습 37 최빈값 map 적용해보기(나머지 로직 보완)

--- src/study/practice/practice36/ChineseRestaurant.java
+++ src/study/practice/practice36/ChineseRestaurant.java
@@ -1,13 +1,21 @@
 package study.practice.practice36;
 
+import java.util.ArrayList;
+import java.util.List;
+
 //Base코드
 public class ChineseRestaurant {
 	public static void main(String[] args) {
 		Menu jjajang = new Menu("짜장", 4900);
 		Menu jjambbong = new Menu("짬뽕", 5900);
 		Menu tangsook = new Menu("탕수육", 13900);
-		Menu[] menuArr = { jjajang, jjambbong, tangsook };
-		Order order = new Order(123, menuArr);
+
+		List<Menu> menuList = new ArrayList<>();
+        menuList.add(jjajang);
+        menuList.add(jjambbong);
+        menuList.add(tangsook);
+        
+		Order order = new Order(123, menuList);
 		System.out.printf("주문 합계: %d원\n", order.totalPrice());
 	}
 }
--- src/study/practice/practice36/Order.java
+++ src/study/practice/practice36/Order.java
@@ -1,18 +1,22 @@
 package study.practice.practice36;
 
+import java.util.List;
+
 class Order {
-	int orderNum;
-	Menu[] menus;
-	public Order(int i, Menu[] arr) {
-		orderNum = i;
-		menus = arr;
-	}  
-	public int totalPrice() {
-		int total=0;
-		for(int i=0; i<menus.length; i++) {
-			Menu menu = menus[i];
-	        total += menu.price;
-		}
-		return total;
-	}
+    int orderNum;
+    List<Menu> menus;
+
+    // 생성자
+    public Order(int i, List<Menu> list) { 
+        orderNum = i;
+        menus = list;
+    }
+
+    public int totalPrice() {
+        int total = 0;
+        for (Menu menu : menus) {
+            total += menu.price;
+        }
+        return total;
+    }
 }
