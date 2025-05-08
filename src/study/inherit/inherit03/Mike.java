8bb3a2bd8c7fd039bbd175c3369a463f23fc90e4
KJW-623 <kimjiwoo4056@gmail.com>
2025-05-08 16:57:23 +0900

    클래스 상속 타입변환, 메소드 재정의, 다형성

---
+++ src/study/inherit/inherit03/Mike.java
@@ -0,0 +1,16 @@
+package study.inherit.inherit03;
+
+public class Mike {
+
+	String model;
+	int price;
+	String type;
+	
+	public void volumeUp() {
+		System.out.println("Mike volumeUp");
+	}
+	
+	public void check() {
+		System.out.println("마이크 기능 확인");
+	}
+}
