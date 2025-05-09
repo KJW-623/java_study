3fd04b3bd8869af6c7650c07b1e8c9dde8267535
KJW-623 <kimjiwoo4056@gmail.com>
2025-05-09 14:02:42 +0900

    클래스 상속 다형성의 개념 예시

---
+++ src/study/inherit/inherit05/v2/Cleaner.java
@@ -0,0 +1,13 @@
+package study.inherit.inherit05.v2;
+
+public class Cleaner {
+	public void washWindow() {
+		System.out.println("cleaner 창문닦기");
+	}
+	public void removeDust() {
+		System.out.println("cleaner 먼지털어");
+	}
+	public void cleanFloor() {
+		System.out.println("cleaner 바닥쓸기");
+	}
+}
