872f7b9fc6187573fe6957fc8a91da554772c9ac
KJW-623 <kimjiwoo4056@gmail.com>
2025-05-09 09:29:07 +0900

    연습 40~42 상속

---
+++ src/study/practice/practice40/Wizard.java
@@ -0,0 +1,11 @@
+package study.practice.practice40;
+
+class Wizard extends Novice {
+    /* 2. mp 필드를 추가하세요. */
+    int mp;
+
+    /* 3. fireball() 메소드를 만드세요. */
+    public void fireball() {
+        System.out.printf("[%s]의 파이어볼@@\n", name);
+    }
+}
